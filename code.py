ebb5b0785dafd3777a1eb6a81eff216439c1a571
zalnemer2287 <57548550+zalnemer2287@users.noreply.github.com>
2022-01-21 19:42:35 -0500

    Solving a conflict

--- code.py
+++ code.py
@@ -1,22 +1,6 @@
-<<<<<<< Updated upstream
 def are_valid_groups (studentNums, groups):
     for x in groups:
         for y in x:
             if not (y in studentNums):
                 return False
     return True
-=======
-def are_valid_groups(student_nums, groups):
-    for i in student_nums:
-        found = False
-        for j in groups:
-            for k in j:
-                if (i == k):
-                    found = True
-        if (found):
-            pass
-        else:
-            return False
-
-    return True
->>>>>>> Stashed changes
