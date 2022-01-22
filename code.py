4375b8b3a96bf9adc0784564fd425a7c35f589b3
zalnemer2287 <57548550+zalnemer2287@users.noreply.github.com>
2022-01-21 19:40:51 -0500

    Correcting my function

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 def are_valid_groups (studentNums, groups):
     for x in groups:
         for y in x:
-            if not (x in studentNums):
+            if not (y in studentNums):
                 return False
     return True
